322b4d7a99393a6bbb3a81d79b30cb271ae96897
Serge Shekhavtsov <sergesh@rambler.ru>
2024-12-27 00:16:57 +0300

    Fix indentation. Fix methods' signatures.

--- console/chargemem.cpp
+++ console/chargemem.cpp
@@ -1,16 +1,17 @@
+#include <cstdio>
 #include <cstdlib>
-#include <string.h>
-#include <iostream>
+#include <cstring>
+
 #include "cascade.h"
 #include "chargemem.h"
 
 CHARGE_MEM::CHARGE_MEM(int SIZE_MEM)
-: CASCADE(), IN(NULL)
+: CASCADE(), IN(nullptr)
 {
-   SIZE = SIZE_MEM;
-   C = new char[SIZE_MEM];
-   Cbuff = new char[SIZE_MEM];
-   memset(C, '0', SIZE_MEM);
+    SIZE = SIZE_MEM;
+    C = new char[SIZE_MEM];
+    Cbuff = new char[SIZE_MEM];
+    memset(C, '0', SIZE_MEM);
 }
 
 CHARGE_MEM::~CHARGE_MEM()
@@ -21,49 +22,49 @@ CHARGE_MEM::~CHARGE_MEM()
 
 void CHARGE_MEM::ClearBuff(void)
 {
-	memset(Cbuff, '0', SIZE);
+    memset(Cbuff, '0', SIZE);
 }
 
 void CHARGE_MEM::ClearData(void)
 {
-	memset(C, '0', SIZE);
+    memset(C, '0', SIZE);
 }
 
 void CHARGE_MEM::CopyBuff(void)
 {
-	memcpy(Cbuff, C, SIZE);
+    memcpy(Cbuff, C, SIZE);
 }
 
 void CHARGE_MEM::CopyData(void)
 {
-	memcpy(&C[1], Cbuff, SIZE-1);
-	if(IN != NULL) C[0] = IN->get();
+    memcpy(&C[1], Cbuff, SIZE-1);
+    if(IN != nullptr) C[0] = IN->get();
 }
 
 void CHARGE_MEM::clk(int phase)
 {
-	switch(phase)
-	{
-		case 0: ClearBuff(); break;
-		case 1: CopyBuff(); break;
-		case 2: ClearData(); break;
-		case 3: CopyData();
-	}
+    switch(phase)
+    {
+        case 0: ClearBuff(); break;
+        case 1: CopyBuff(); break;
+        case 2: ClearData(); break;
+        case 3: CopyData();
+    }
 }
 
-char CHARGE_MEM::get(void)
+char CHARGE_MEM::get(void) const
 {
-	return Cbuff[SIZE-1];
+    return Cbuff[SIZE-1];
 }
 
 void CHARGE_MEM::cascade(CASCADE *object)
 {
-	IN = object;
+    IN = object;
 }
 
 void CHARGE_MEM::set(int A, char D)
 {
-  C[A] = D;
+    C[A] = D;
 }
 
 int CHARGE_MEM::Size(void)
@@ -73,8 +74,8 @@ int CHARGE_MEM::Size(void)
 
 void CHARGE_MEM::show(void)
 {
- char tmp = C[SIZE-1];
- C[SIZE-1] = 0;
- printf("%s%c\n",C,tmp);
- C[SIZE-1] = tmp;
+    char tmp = C[SIZE-1];
+    C[SIZE-1] = 0;
+    printf("%s%c\n",C,tmp);
+    C[SIZE-1] = tmp;
 }
--- console/k145_com.cpp
+++ console/k145_com.cpp
@@ -1,42 +1,41 @@
-#include <cstdlib>
 #include "cascade.h"
 #include "k145_com.h"
 
 const char *unknown_chip  = "unknown chip";
 
 K145::K145()
-: CASCADE(), BUSi(NULL), BUSo(NULL), M(NULL)
-{	
+: CASCADE(), BUSi(nullptr), BUSo(nullptr), M(nullptr)
+{
 }
 
 K145::~K145()
 {
-     if(M != NULL) delete M;
+    delete M;
 }
-            
+
 
 void K145::clk(int phase)
 {
 }
 
-const char* K145::about(void)
+const char* K145::about(void) const
 {
-	return unknown_chip;
+    return unknown_chip;
 }
 
 K145* K145::Target(void)
 {
-	return BUSo;
+    return BUSo;
 }
 
 void K145::out_to(K145 *ms)
 {
-	BUSo = ms;
-	ms->BUSi = this;
-	ms->M->cascade(this);
+    BUSo = ms;
+    ms->BUSi = this;
+    ms->M->cascade(this);
 }
 
-char K145::get(void)
+char K145::get(void) const
 {
-	return M->get();
+    return M->get();
 }
